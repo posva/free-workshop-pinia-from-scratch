7944359336ef011f26fe669d16da7847acce8403
Eduardo San Martin Morote <posva13@gmail.com>
2024-04-22 14:54:03 +0200

    remove warning

--- code/src/views/index.vue
+++ code/src/views/index.vue
@@ -2,26 +2,10 @@
 import { useExerciseLinks } from '../router'
 
 const exerciseLinks = useExerciseLinks()
-const showReviewWarning = import.meta.env.VITE_GIT_CLONE_WARNING !== 'off'
 </script>
 
 <template>
   <h2 class="mb-6">Exercises:</h2>
-
-  <div
-    v-if="showReviewWarning"
-    class="rounded-md bg-red-200 dark:bg-red-800 border dark:border-red-50 border-red-950 py-2 px-6 mb-12"
-  >
-    <h3>😅 Oops!</h3>
-    <img src="/oops.gif" alt="Oh, wow!" class="max-h-24" />
-    <p class="m-0 text-lg">⚠️ It looks like you <i>cloned the repository directly</i>.</p>
-
-    <p class="text-lg">You need to run this command instead:</p>
-
-    <pre><code>npx zx@7.2 https://gist.githubusercontent.com/posva/d19708c1da18d41d66ac7cec1a1e5557/raw/bootstrap.mjs</code></pre>
-
-    <p>Delete this project and try again with that command! 🙌</p>
-  </div>
 
   <ul>
     <li v-for="link in exerciseLinks" :key="link.name">
